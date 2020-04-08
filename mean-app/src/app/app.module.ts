1b91770456748a9008168b5aae5d56e5b84e7929
Swizzle <miringanita@gmail.com>
2020-04-08 20:21:41 +0200

    adding Angular Materials

--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
