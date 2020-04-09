14489f460c6061618b359feb0da20cb6b8a65b74
Swizzle <miringanita@gmail.com>
2020-04-09 23:39:52 +0200

    getting shape

--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -18,7 +20,9 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
