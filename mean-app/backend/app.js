25edd4068f162eef2d505322b9d523710fa7f2b2
Swizzle <miringanita@gmail.com>
2020-04-16 08:39:32 +0200

    adding nodemon and fetching firs post

--- mean-app/backend/app.js
+++ mean-app/backend/app.js
@@ -3,12 +3,24 @@ const express = require('express');
  const app = express();
 
  app.use((req, res, next) => {
+   const posts = [
+     {
+       id: 'dvobneoep4203',
+       title: 'First server-side post',
+       content: 'This is comming from server!'
+     },
+     {
+      id: 'h9wh49hoiqh0f',
+      title: 'Second server-side post',
+      content: 'This is comming from server!'
+    },
+   ];
    console.log('First middleware');
-   next();
+   res.status(200).json({
+     message: 'Posts fetched succesfully',
+     posts: posts
+   });
  });
 
- app.use((req, res, next) => {
-   res.send('Hello from express!');
- })
 
  module.exports = app;
